1e2619bd50944b87a970cda57c1c7da678a142da
Bobby Galli <bobbyg603@gmail.com>
2017-09-09 13:05:24 -0400

    Removed unused zone parameter. Minor formatting changes.

--- src/bugsplat-error-handler.ts
+++ src/bugsplat-error-handler.ts
@@ -1,4 +1,4 @@
-import { ErrorHandler, Injectable, Inject, InjectionToken, NgZone } from '@angular/core';
+import { ErrorHandler, Injectable, Inject, InjectionToken } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
 export let BugSplatConfigToken = new InjectionToken<BugSplatConfig>('bugsplat.config');
@@ -10,7 +10,7 @@ export interface BugSplatConfig {
 
 @Injectable()
 export class BugSplatErrorHandler implements ErrorHandler {
-  constructor(@Inject(BugSplatConfigToken) private config: BugSplatConfig, private http: HttpClient, private zone: NgZone) { }
+  constructor( @Inject(BugSplatConfigToken) private config: BugSplatConfig, private http: HttpClient) { }
   handleError(error) {
     console.log('Exception caught by BugSplat!')
     console.log('BugSplat AppName:', this.config.appName);
@@ -29,10 +29,8 @@ export class BugSplat {
   private callback: Function = null;
   private callbackContext: Object = null;
   private files: File[] = [];
-  
-  constructor(private config: BugSplatConfig,
-    private http: HttpClient
-  ) { }
+
+  constructor(private config: BugSplatConfig, private http: HttpClient) { }
 
   post(error) {
     const url = "https://" + this.config.database + ".bugsplat.com/post/js/";
@@ -49,33 +47,30 @@ export class BugSplat {
     this.files.forEach(file => {
       body.append(file.name, file, file.name);
     });
-    this.http.post(url, body).subscribe(
-      data => {
-        console.log("BugSplat POST Success:", data);
-        if(this.callback !== null) {
-          this.callback(null, data, this.callbackContext);
-        }
-      },
-      err => {
-        console.log("BugSplat POST Error:", err);
-        if(this.callback !== null) {
-          this.callback(err, null, this.callbackContext);
-        }
+    this.http.post(url, body).subscribe(data => {
+      console.log("BugSplat POST Success:", data);
+      if (this.callback !== null) {
+        this.callback(null, data, this.callbackContext);
       }
-    );
+    }, err => {
+      console.log("BugSplat POST Error:", err);
+      if (this.callback !== null) {
+        this.callback(err, null, this.callbackContext);
+      }
+    });
   }
 
   addAddtionalFile(file: File) {
     const currentUploadSize = this.files.reduce((previous, current) => { return previous + current.size; }, 0);
     const newUploadSize = currentUploadSize + file.size;
-    if(newUploadSize >= 2 * 1024 * 1024) {
+    if (newUploadSize >= 2 * 1024 * 1024) {
       console.error("BugSplat Error: Could not add file " + file.name + ". Upload bundle size limit exceeded!");
     } else {
       this.files.push(file);
     }
   }
 
-  // TODO type check context?
+  // TODO BG type check context?
   setCallback(callback: Function, context?: Object) {
     this.callback = callback;
     this.callbackContext = context;
