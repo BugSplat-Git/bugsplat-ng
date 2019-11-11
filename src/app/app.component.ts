d5cceb6519b7ec85321e8582f0db376f006a2ac8
Bobby Galli <bobbyg603@gmail.com>
2019-11-11 08:19:00 -0500

    Updated my-angular-crasher sample.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,48 @@
-import { Component, ErrorHandler, OnInit, OnDestroy } from '@angular/core';
-import { MyAngularErrorHandler } from "./my-angular-error-handler";
-import { Subscription } from 'rxjs';
+import { Component, ErrorHandler, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { MyAngularErrorHandler } from './my-angular-error-handler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
 
-  title = "my-angular-crasher";
-
-  crashId = "";
-  database = "";
-  link = "https://www.bugsplat.com";
-  linkText = "";
-  logs = "";
-
-  bugSplatEventSubscription: Subscription;
+  title: string = 'my-angular-crasher';
+  database: string = '';
+  logEntries: Array<string> = [];
+  
+  crashId$: Observable<number>;
+  crashLink$: Observable<Link>;
 
   constructor(private errorHandler: ErrorHandler) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     const myAngularErrorHandler = (<MyAngularErrorHandler>this.errorHandler);
-    this.bugSplatEventSubscription = myAngularErrorHandler.bugsplat.getObservable()
-      .subscribe((event) => {
-        this.database = myAngularErrorHandler.config.database;
-        this.crashId = "" + event.responseData.crash_id;
-        this.link = "https://app.bugsplat.com/individualCrash?database=" + this.database + "&id=" + this.crashId;
-        this.linkText = "Crash " + this.crashId + " in database " + this.database;
-      });
+    const bugSplat$ = myAngularErrorHandler.bugsplat.getObservable();
+
+    this.database = myAngularErrorHandler.config.database;
+    this.crashId$ = bugSplat$.pipe(map(bugSplatEvent => bugSplatEvent.responseData.crash_id));
+    this.crashLink$ = this.crashId$.pipe(map(crashId => {
+      return {
+        href: `https://app.bugsplat.com/v2/crash?database=${this.database}&id=${crashId}`,
+        text: `Crash ${crashId} in database ${this.database}`
+      };
+    }));
   }
 
-  ngOnDestroy(): void {
-    if (this.bugSplatEventSubscription) {
-      this.bugSplatEventSubscription.unsubscribe();
-    }
-  }
-
-  myEvent(event) {
-    const error = new Error("foobar!");
-    this.logs += "BugSplat!" + "<br/>" + error.message + "<br/>" + error.stack + "<br/><br/>";
+  onButtonClick(): void {
+    const error = new Error('Crush your bugs!');
+    this.logEntries.push('BugSplat!');
+    this.logEntries.push(error.message);
+    this.logEntries.push(error.stack);
     throw error;
   }
+}
+
+interface Link {
+  text: string;
+  href: string;
 }
