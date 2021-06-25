c3006f3b2439154118ae2bf5cffc51fd866eb5b3
Bobby Galli <bobbyg603@gmail.com>
2021-06-25 16:57:46 -0400

    Removes unused string

--- projects/bugsplat-ng/src/lib/bugsplat-error-handler.ts
+++ projects/bugsplat-ng/src/lib/bugsplat-error-handler.ts
@@ -3,8 +3,6 @@ import { BugSplat } from "./bugsplat";
 
 @Injectable()
 export class BugSplatErrorHandler implements ErrorHandler {
-  static readonly ERROR_CANNOT_BE_NULL = "BugSplatErrorHandler Error: handleError was passed a null error!";
-
   constructor(public bugsplat: BugSplat) { }
 
   async handleError(error: Error): Promise<void> {
