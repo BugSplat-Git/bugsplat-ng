80312cca0d3149b4887216fa7109689e4eef32a7
BugSplatService <support@bugsplat.com>
2025-11-12 21:47:39 +0000

    chore: update angular cli

--- projects/my-angular-crasher/src/app/app.component.ts
+++ projects/my-angular-crasher/src/app/app.component.ts
@@ -3,14 +3,14 @@ import { toSignal } from '@angular/core/rxjs-interop';
 import { BugSplatErrorHandler } from 'bugsplat-ng';
 import { map } from 'rxjs/operators';
 import { MyAngularErrorHandler } from './my-angular-error-handler';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     standalone: true,
-    imports: [CommonModule]
+    imports: []
 })
 export class AppComponent implements OnInit {
   title = 'my-angular-crasher';
